6ff765a343a0d254c0be545d6ca41ab549a98eca
Oluseyi Adeegbe <adeegbeoluseyi@gmail.com>
2022-09-10 17:28:44 +0100

    LSP

--- liskov_substitution_principle.js
+++ liskov_substitution_principle.js
@@ -1,3 +1,11 @@
+// The Liskov Substitution Principle (LSP) is one of five SOLID 
+// object-oriented design principles. It states that a superclass object should be 
+// replaceable with a subclass object without breaking the functionality of the software.
+
+// This principle ensures that inheritance (one of the OOP principles) is used correctly. 
+// If an override method does nothing or just throws an exception, then you're probably violating the LSP. 
+// The classic example of the inheritance technique causing problems is the circle-elipse problem (a.k.a the rectangle-square problem) which is a is a violation of the Liskov substitution principle.
+
 class Rectangle{
     constructor(width, height){
         this.width = width;
